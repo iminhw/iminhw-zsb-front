1f8d3ae9865e4f6dfcec6808024a85c1e5df6cfb
凉伊 <iminhw@qq.com>
2022-07-15 14:13:57 +0800

    first

--- src/main/java/com/minhw/entity/StuOutNotification.java
+++ src/main/java/com/minhw/entity/StuOutNotification.java
@@ -1,48 +1,18 @@
 package com.minhw.entity;
 
 import java.io.Serializable;
-import java.util.Date;
 
 public class StuOutNotification implements Serializable {
     private String ksh;
-
     private String kddh;
-
     private String xm;
-
-    private String yzbh;
-
-    private String lxdh;
-
-    private String yjdz;
-
-    private Byte status;
-
-    private String createBy;
-
-    private String updateBy;
-
-    private String remark;
-
-    private Date updateTime;
-
-    private Date createTime;
 
     private static final long serialVersionUID = 1L;
 
-    public StuOutNotification(String ksh, String kddh, String xm, String yzbh, String lxdh, String yjdz, Byte status, String createBy, String updateBy, String remark, Date updateTime, Date createTime) {
+    public StuOutNotification(String ksh, String kddh, String xm) {
         this.ksh = ksh;
         this.kddh = kddh;
         this.xm = xm;
-        this.yzbh = yzbh;
-        this.lxdh = lxdh;
-        this.yjdz = yjdz;
-        this.status = status;
-        this.createBy = createBy;
-        this.updateBy = updateBy;
-        this.remark = remark;
-        this.updateTime = updateTime;
-        this.createTime = createTime;
     }
 
     public StuOutNotification() {
@@ -73,75 +43,4 @@ public class StuOutNotification implements Serializable {
         this.xm = xm == null ? null : xm.trim();
     }
 
-    public String getYzbh() {
-        return yzbh;
-    }
-
-    public void setYzbh(String yzbh) {
-        this.yzbh = yzbh == null ? null : yzbh.trim();
-    }
-
-    public String getLxdh() {
-        return lxdh;
-    }
-
-    public void setLxdh(String lxdh) {
-        this.lxdh = lxdh == null ? null : lxdh.trim();
-    }
-
-    public String getYjdz() {
-        return yjdz;
-    }
-
-    public void setYjdz(String yjdz) {
-        this.yjdz = yjdz == null ? null : yjdz.trim();
-    }
-
-    public Byte getStatus() {
-        return status;
-    }
-
-    public void setStatus(Byte status) {
-        this.status = status;
-    }
-
-    public String getCreateBy() {
-        return createBy;
-    }
-
-    public void setCreateBy(String createBy) {
-        this.createBy = createBy == null ? null : createBy.trim();
-    }
-
-    public String getUpdateBy() {
-        return updateBy;
-    }
-
-    public void setUpdateBy(String updateBy) {
-        this.updateBy = updateBy == null ? null : updateBy.trim();
-    }
-
-    public String getRemark() {
-        return remark;
-    }
-
-    public void setRemark(String remark) {
-        this.remark = remark == null ? null : remark.trim();
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
 }
