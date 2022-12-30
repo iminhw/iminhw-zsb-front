ad7d2f8d80caf0a2affbd35373bb5565fe923c67
minhongwei <minhongwei@wiseasy.com>
2022-12-30 14:44:18 +0800

    修改

--- src/main/java/com/minhw/entity/StuOutNotification.java
+++ src/main/java/com/minhw/entity/StuOutNotification.java
@@ -10,10 +10,11 @@ public class StuOutNotification implements Serializable {
     @Size(min = 8, message = "考生号格式错误")
     @Pattern(regexp = "^[0-9]*$", message = "考生号格式错误")
     private String ksh;
-    @NotEmpty(message = "身份证号不能为空")
+//    @NotEmpty(message = "身份证号不能为空")
     @Pattern(regexp = "^([1-6][1-9]|50)\\d{4}(18|19|20)\\d{2}((0[1-9])|10|11|12)(([0-2][1-9])|10|20|30|31)\\d{3}[0-9Xx]$", message = "身份证号格式错误")
     private String sfzh;
     private String kddh;
+    @NotEmpty(message = "姓名不能为空")
     private String xm;
 
 
